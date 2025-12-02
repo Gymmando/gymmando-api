2a6803509c1a8a4987e705547592e3d77a581c12
Abdu Radi <92768651+AbduKhRadi@users.noreply.github.com>
2025-12-01 20:25:54 -0500

    formatting the files

--- api/api.py
+++ api/api.py
@@ -1,28 +1,25 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from livekit import api
-import os
-from dotenv import load_dotenv
 
 load_dotenv()
 
 app = FastAPI()
 
+
 def create_livekit_token():
     token = api.AccessToken(
-        api_key=os.getenv("LIVEKIT_API_KEY"),
-        api_secret=os.getenv("LIVEKIT_API_SECRET")
+        api_key=os.getenv("LIVEKIT_API_KEY"), api_secret=os.getenv("LIVEKIT_API_SECRET")
     )
-    
+
     token.with_identity("user_123")
     token.with_name("Gym User")
-    token.with_grants(
-        api.VideoGrants(
-            room_join=True,
-            room="gym-room"
-        )
-    )
-    
+    token.with_grants(api.VideoGrants(room_join=True, room="gym-room"))
+
     return token.to_jwt()
+
 
 @app.get("/token")
 def get_token():
